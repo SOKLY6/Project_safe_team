d53fa8bb5244edf34113ee08251fae06bcd3956f
moonshy <vladbobkb10@gmail.com>
2025-10-11 23:53:16 +0300

    docs: edit .gitignore

--- FastAPI/backend/test_api_client.py
+++ FastAPI/backend/test_api_client.py
@@ -1,21 +1,16 @@
 from .database import SessionLocal
 from . import models
 
-# Создаем сессию
 db = SessionLocal()
 
-# Создаем объекты организаций
 org1 = models.Organization(name="safe_team")
 org2 = models.Organization(name="dev_team")
 org3 = models.Organization(name="qa_team")
 
-# Добавляем в сессию
 db.add_all([org1, org2, org3])
 
-# Сохраняем изменения
 db.commit()
 
-# Закрываем соединение
 db.close()
 
 print("✅ Тестовые данные добавлены в таблицу organizations.")
