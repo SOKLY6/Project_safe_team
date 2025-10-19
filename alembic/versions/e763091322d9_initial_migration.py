ec72898b1d4eac43620c4f8fe65c53e2eb33ef13
moonshy <vladbobkb10@gmail.com>
2025-10-20 01:35:26 +0300

    docs: .gitignore migrations

--- alembic/versions/e763091322d9_initial_migration.py
+++ alembic/versions/e763091322d9_initial_migration.py
@@ -8,8 +8,8 @@ Create Date: 2025-10-20 01:21:18.623727
 
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+# from alembic import op
+# import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
